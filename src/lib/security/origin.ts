85def1fc6c2f9704bcb4ddd0893ceffb4d8cdb24
Ernur-man <ernuraman926@gmail.com>
2026-06-24 15:50:29 +0500

    sourteenth

--- src/lib/security/origin.ts
+++ src/lib/security/origin.ts
@@ -6,6 +6,7 @@ function getAllowedOrigins(): string[] {
     siteUrl,
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://top-git-main-era8.vercel.app/"
   ]);
 
   if (siteUrl.startsWith("https://")) {
